178f85ebf99cc2eeb14f58f4445ae5b229115865
Giacomo Deodato <34250006+giacomodeodato@users.noreply.github.com>
2021-03-03 16:42:55 +0100

    add training notebooks

---
+++ code/LeNet5.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv3 = nn.Conv2d(16, 120, 5)
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
+        
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.relu(self.conv3(x))
+        x = torch.flatten(x, 1, -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
